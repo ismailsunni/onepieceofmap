7c7cb615170cf8d8894cbe6b8eaf2bfc8f9ba58a
Ismail Sunni <imajimatika@gmail.com>
2023-11-14 23:06:40 +0700

    First commit.

---
+++ script.js
@@ -0,0 +1,27 @@
+var map = L.map("map").setView([0, 0], 2);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
+
+// Load GeoJSON data
+fetch("data.geojson")
+  .then((response) => response.json())
+  .then((data) => {
+    L.geoJSON(data, {
+      onEachFeature: function (feature, layer) {
+        // Create a popup with feature properties
+        var popupContent = `
+        <div class="popup">
+            <h3>${feature.properties.name}</h3>
+            <ul>
+                <li><b>One Piece Wikia:</b> <a href='${feature.properties.onePieceWikia}' target='_blank'>${feature.properties.name}</a></li>
+                <li><b>Real World Location:</b> ${feature.properties.realWorldLocation}</li>
+                <li><b>Wikipedia:</b> <a href='${feature.properties.wikipedia}' target='_blank'>Link</a></li>
+            </ul>
+        </div>`;
+
+        layer.bindPopup(popupContent);
+      },
+    }).addTo(map);
+  });
